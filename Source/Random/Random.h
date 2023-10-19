9574d595ec6193128bcc21845a663522fdc48289
Diegothic <diegothic24@gmail.com>
2023-10-19 12:31:16 +0200

    Pseudo-Random Number Generation

---
+++ Source/Random/Random.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <cstdint>
+
+#include "LCG.h"
+
+namespace Arg
+{
+	class Random
+	{
+	public:
+		Random(uint64_t seed = 0);
+		~Random() = default;
+
+		uint64_t GetInitialSeed() const { return m_InitialSeed; }
+		uint64_t GetSeed() const;
+		void SetSeed(uint64_t seed);
+
+		uint64_t Next();
+		int32_t NextInt();
+
+	private:
+		LCG m_LCG;
+		uint64_t m_InitialSeed;
+	};
+}
